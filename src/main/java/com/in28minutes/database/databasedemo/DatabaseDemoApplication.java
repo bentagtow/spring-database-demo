420b92ccc6f405b79d5bcd4c26354b98fa76b38f
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-02 16:57:27 -0600

    adds insert method

--- src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class DatabaseDemoApplication implements CommandLineRunner {
 
@@ -25,5 +27,8 @@ public class DatabaseDemoApplication implements CommandLineRunner {
 		logger.info("All users -> {}", dao.findAll());
 		logger.info("User id 10011 -> {}", dao.findById(10011));
 		logger.info("Deleting 10012 -> Number of Rows Deleted - {}", dao.deleteById(10012));
+		logger.info("Inserting 10014 -> {}", dao.insert(new Person(10014, "Benji", "Berlin", new Date())));
+		logger.info("Update 10013 -> {}",
+				dao.update(new Person(10013, "Ben", "Ten", new Date())));
 	}
 }
