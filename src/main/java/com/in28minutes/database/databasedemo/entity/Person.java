55d36ab31d5b5e238f2bb6084513584370360746
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-02 13:43:35 -0600

    finds all people and prints to console

--- src/main/java/com/in28minutes/database/databasedemo/entity/Person.java
+++ src/main/java/com/in28minutes/database/databasedemo/entity/Person.java
@@ -8,6 +8,10 @@ public class Person {
     private String location;
     private Date birthDate;
 
+    public Person() {
+        //no argument constructor
+    }
+
     public Person(int id, String name, String location, Date birthDate) {
         super();
         this.id = id;
@@ -46,5 +50,15 @@ public class Person {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPerson{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
     }
 }
